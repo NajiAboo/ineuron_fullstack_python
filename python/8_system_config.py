e58b6580b4baf66796b82997c58ce298c887817c
t0393 <naji.aboo@triassicsolutions.com>
2022-01-01 19:33:56 +0530

    system config

---
+++ python/8_system_config.py
@@ -0,0 +1,6 @@
+import platform
+
+print(f"Platform : {platform.machine()}")
+print(f"VErsion : {platform.version()}")
+print(f"Platform : {platform.platform()}")
+print(f"Processor : {platform.processor()}")
